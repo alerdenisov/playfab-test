50c80c86f4df7a7c935893a9b76406e2496abec4
Aler Denisov <aler.zampillo@gmail.com>
2016-03-06 23:47:19 +0300

    auto push app.js

--- app.js
+++ app.js
@@ -1,3 +1,65 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var _this = this;
+var SyncableObject = (function () {
+    function SyncableObject() {
+    }
+    SyncableObject.prototype.Straight = function () {
+        var keys = Object.keys(this);
+        var straight = {};
+        for (var i = 0; i < keys.length; i++) {
+            straight[this.ObjectKey() + "_" + keys[i]] = this[keys[i]];
+        }
+        return straight;
+    };
+    SyncableObject.prototype.Serialize = function () {
+        return JSON.stringify(this.Straight());
+    };
+    SyncableObject.prototype.Deserialize = function (input) {
+        var keys = Object.keys(this);
+        var updated = {};
+        for (var i = 0; i < keys.length; i++) {
+            if (input.Data[this.ObjectKey() + "_" + keys[i]]) {
+                this[keys[i]] = input.Data[this.ObjectKey() + "_" + keys[i]].Value;
+            }
+            else {
+                updated[this.ObjectKey() + "_" + keys[i]] = this[keys[i]];
+            }
+        }
+        return updated;
+    };
+    return SyncableObject;
+})();
+var PlayerObject = (function (_super) {
+    __extends(PlayerObject, _super);
+    function PlayerObject() {
+        _super.apply(this, arguments);
+        this.Exp = 0;
+        this.Gold = 0;
+        this.Level = 1;
+        this.Silver = 0;
+        this.Airplanes = 5;
+        this.Name = "Unnamed";
+    }
+    PlayerObject.prototype.ObjectKey = function () { return "Player"; };
+    return PlayerObject;
+})(SyncableObject);
+var AirplaneObject = (function (_super) {
+    __extends(AirplaneObject, _super);
+    function AirplaneObject() {
+        _super.apply(this, arguments);
+        this.Rank = 0;
+        this.Speed = 0;
+        this.Durability = 0;
+        this.Movement = 0;
+        this.AvailablePoint = 0;
+    }
+    AirplaneObject.prototype.ObjectKey = function () { return "Airplane"; };
+    return AirplaneObject;
+})(SyncableObject);
 var DataRequest = (function () {
     function DataRequest(PlayFabId) {
         this.PlayFabId = PlayFabId;
@@ -23,34 +85,11 @@ var DataRequest = (function () {
 var PlayerData = function (id) {
     // Check defaults
     // 1) Load read only data
-    var playerReadOnly = server.GetUserReadOnlyData(new DataRequest(id).With("Level", "Airplanes", "Exp", "Gold", "Silver", "NotFirstTime", "ForceReinit").Build());
-    var playerFieldsDefaults = {
-        "Level": "1",
-        "Airplanes": "5",
-        "Exp": "1",
-        "Gold": "1",
-        "Silver": "1"
-    };
-    var result = {};
-    var update = {};
-    var force = !playerReadOnly.Data["NotFirstTime"] || !!playerReadOnly.Data["ForceReinit"];
-    var CheckOrDefault = function (key, value) {
-        if (force || !playerReadOnly.Data[key]) {
-            result[key] = update[key] = value;
-        }
-        else if (playerReadOnly.Data[key]) {
-            result[key] = playerReadOnly.Data[key].Value;
-        }
-        else {
-            throw "1@!3123";
-        }
-    };
-    var keys = Object.keys(playerFieldsDefaults);
-    for (var i = 0; i < keys.length; i++) {
-        var key = keys[i];
-        var value = playerFieldsDefaults[key];
-        CheckOrDefault(key, value);
-    }
+    var player = new PlayerObject();
+    var request = { PlayFabId: _this };
+    var playerReadOnly = server.GetUserReadOnlyData(request);
+    var update = player.Deserialize(playerReadOnly);
+    var result = player.Straight();
     update["NotFirstTime"] = "1";
     update["ForceReinit"] = "";
     server.UpdateUserReadOnlyData({
